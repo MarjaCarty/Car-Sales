19670d59296134a436abdba96ae55a712a0e5cce
MarjaCarty <marja.d.carty@gmail.com>
2020-11-03 15:50:42 -0600

    Add state to all components that need it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-const Header = props => {
+const Header = ({ image, name, price }) => {
   return (
     <>
       <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+        <img src={image} alt={name} />
       </figure>
-      <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <h2>{name}</h2>
+      <p>Amount: ${price}</p>
     </>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  const { image, name, price } = state.car;
+  return {
+    image,
+    name,
+    price,
+  };
+};
+
+export default connect(mapStateToProps, {})(Header);
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,11 +1,19 @@
-// import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-// const Total = props => {
-//   return (
-//     <div className="content">
-//       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
-//     </div>
-//   );
-// };
+const Total = ({ price, addPrice }) => {
+  return (
+    <div className="content">
+      <h4>Total Amount: ${price + addPrice}</h4>
+    </div>
+  );
+};
 
-// export default Total;
+const mapStateToProps = (state) => {
+  return {
+    price: state.car.price,
+    addPrice: state.additionalPrice,
+  };
+};
+
+export default connect(mapStateToProps, {})(Total);
